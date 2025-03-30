af6b421204ab862ee7f99a7b52cfaf63f410ec7b
Jadenpy hu <63620612@qq.com>
2025-03-30 14:18:47 +0000

    Save progress: 200.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo hello questionnaire
 ls -l
-QUESTION1="what's your name?"
+#QUESTION1="what's your name? 1"
 QUESTION1="What's your name?"
 echo $QUESTION1
+read NAME
+
+echo $NAME
