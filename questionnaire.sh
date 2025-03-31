9d157bb5da7062acb12fcad786e8d29787cb2dd7
Jadenpy hu <63620612@qq.com>
2025-03-31 12:12:03 +0000

    Save progress: 548.1

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-#Title
 echo -e "\n~~ Questionnaire ~~\n"
 
-#Question1
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
+
 echo $QUESTION1
 read NAME
-echo "Hello $NAME."
 
-#Question2
-QUESTION2="Where are you from?"
 echo $QUESTION2
 read LOCATION
-echo "Hello $NAME from $LOCATION."
 
-#QUESTION3
-QUESTION3="What's your favorite coding website?"
 echo $QUESTION3
 read WEBSITE
+
 echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
