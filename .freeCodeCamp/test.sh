659e4bbcd95e41b5275be527af84dea7df099573
Jadenpy hu <63620612@qq.com>
2025-04-01 11:25:19 +0000

    Save progress: 817.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-# Program that counts down to zero from a given argument
+# description:  Bingo Number Generator
 
-#if [[ $1 -gt 0 ]]
-#then
-#  echo true
-#else
-#  echo Include a positive integer as the first argument.
-#fi
+# print title first
+echo -e "\n~~ Bingo Number Generator ~~\n"
 
-echo -e "\n~~ Countdown Timer ~~\n"
+NUMBER=5
 
-: '
-for (( i = $1; i >= 0; i-- ))
-do
-  echo $i
-  sleep 1
-done
-'
 
-I=$1
 
-echo $I
+echo $NUMBER
